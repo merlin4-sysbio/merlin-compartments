1a6cf83107585c1e5e94644f2dcde7965f90c6a4
dlagoa <pg31552@alunos.uminho.pt>
2019-11-19 09:37:00 +0000

    debug

--- src/main/java/pt/uminho/ceb/biosystems/merlin/aibench/operations/loaders/annotation/compartments_new/ChooseToolGUI.java
+++ src/main/java/pt/uminho/ceb/biosystems/merlin/aibench/operations/loaders/annotation/compartments_new/ChooseToolGUI.java
@@ -5,10 +5,6 @@ import java.awt.FlowLayout;
 import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.ItemEvent;
-import java.awt.event.ItemListener;
-import java.io.File;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -17,7 +13,6 @@ import javax.swing.InputMap;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
-import javax.swing.JTextField;
 import javax.swing.KeyStroke;
 
 import org.sing_group.gc4s.dialog.AbstractInputJDialog;
--- src/main/java/pt/uminho/ceb/biosystems/merlin/aibench/operations/loaders/annotation/compartments_new/UrlPopUp.java
+++ src/main/java/pt/uminho/ceb/biosystems/merlin/aibench/operations/loaders/annotation/compartments_new/UrlPopUp.java
@@ -6,7 +6,6 @@ import java.awt.GridBagLayout;
 import java.awt.Insets;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.io.File;
 
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
